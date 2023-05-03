c7e22384ec9a4e9cd91997e6aac2b761ce2b752b
IIISRO <ilgarshukuroff@gmail.com>
2023-05-03 15:06:38 +0400

    'none'

--- core/views.py
+++ core/views.py
@@ -33,61 +33,61 @@ def home(request):
                 i += 1
             token = f'{token}({i})'
             
-        # try:
-        uploadfrom = request.POST.get('uploadfrom')
-        if uploadfrom == 'device':
-            uploaded_video = request.FILES.get('video')
-            if not uploaded_video:
-                return render(request,'home.html',context={'download':False,'error':True,'message':'Please upload a video file.'})
-            if  Path(uploaded_video.name).suffix == '.mp4' or  Path(uploaded_video.name).suffix == '.mov' or  Path(uploaded_video.name).suffix == '.m4a':
-                if uploaded_video.size // 1024 // 1024 > 100:
-                    return render(request,'home.html',context={'download':False,'error':True,'message':'Please select less than 100MB'})    
+        try:
+            uploadfrom = request.POST.get('uploadfrom')
+            if uploadfrom == 'device':
+                uploaded_video = request.FILES.get('video')
+                if not uploaded_video:
+                    return render(request,'home.html',context={'download':False,'error':True,'message':'Please upload a video file.'})
+                if  Path(uploaded_video.name).suffix == '.mp4' or  Path(uploaded_video.name).suffix == '.mov' or  Path(uploaded_video.name).suffix == '.m4a':
+                    if uploaded_video.size // 1024 // 1024 > 100:
+                        return render(request,'home.html',context={'download':False,'error':True,'message':'Please select less than 100MB'})    
 
-                video = mp.VideoFileClip(uploaded_video.temporary_file_path())
-                video.audio.write_audiofile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))             
-                audio = sr.AudioFile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))                
-                r = sr.Recognizer()
-                with audio as source:
-                    r.adjust_for_ambient_noise(source)  
-                    audio_file = r.record(source)
-                text = r.recognize_google(audio_file,language=request.POST.get('lang'))
-                with open(os.path.join(settings.MEDIA_ROOT, f'{token}.txt'),mode ='w',encoding="utf-8") as file: 
-                    wrapped = textwrap.fill(text, 100)
-                    file.write(wrapped)
-                return render(request,'home.html', context={'download':True,'token':token, 'error':False, 'filename':(uploaded_video.name.split('.'))[0]})
-        
-            else:
-                return render(request,'home.html',context={'download':False,'error':True,'message':'Please upload only mp4, mov, m4a.'})
-        elif uploadfrom == 'youtube':
-            ytlink = request.POST.get('video')
-            if not ytlink:
-                return render(request,'home.html',context={'download':False,'error':True,'message':'Please paste the video url.'})
-            else:
-                video = pafy.new(ytlink)
-                
-            if int(video.length) > int(300) :
-                return render(request,'home.html',context={'download':False,'error':True,'message':'Please select less than 5 minute'})
-            else:    
-                bestaudio = video.getbestaudio(preftype='m4a')
-                bestaudio.download(quiet=False, filepath=(f'{settings.MEDIA_ROOT}/{token}.m4a'))
-                raw_audio = mp.AudioFileClip(os.path.join(settings.MEDIA_ROOT, f'{token}.m4a'))
-                raw_audio.write_audiofile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))
-                os.remove(os.path.join(settings.MEDIA_ROOT, f'{token}.m4a'))
-                audio = sr.AudioFile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))
-                r = sr.Recognizer()
-                with audio as source:
-                    r.adjust_for_ambient_noise(source)  
-                    audio_file = r.record(source)
-                text = r.recognize_google(audio_file,language=request.POST.get('lang'))
-                
-                with open(os.path.join(settings.MEDIA_ROOT, f'{token}.txt'),mode ='w',encoding="utf-8") as file: 
-                    wrapped = textwrap.fill(text, 100)
-                    file.write(wrapped)
+                    video = mp.VideoFileClip(uploaded_video.temporary_file_path())
+                    video.audio.write_audiofile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))             
+                    audio = sr.AudioFile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))                
+                    r = sr.Recognizer()
+                    with audio as source:
+                        r.adjust_for_ambient_noise(source)  
+                        audio_file = r.record(source)
+                    text = r.recognize_google(audio_file,language=request.POST.get('lang'))
+                    with open(os.path.join(settings.MEDIA_ROOT, f'{token}.txt'),mode ='w',encoding="utf-8") as file: 
+                        wrapped = textwrap.fill(text, 100)
+                        file.write(wrapped)
+                    return render(request,'home.html', context={'download':True,'token':token, 'error':False, 'filename':(uploaded_video.name.split('.'))[0]})
+            
+                else:
+                    return render(request,'home.html',context={'download':False,'error':True,'message':'Please upload only mp4, mov, m4a.'})
+            elif uploadfrom == 'youtube':
+                ytlink = request.POST.get('video')
+                if not ytlink:
+                    return render(request,'home.html',context={'download':False,'error':True,'message':'Please paste the video url.'})
+                else:
+                    video = pafy.new(ytlink)
                     
-                return render(request,'home.html', context={'download':True,'token':token, 'error':False,'filename': video.title})
-        # except:
-        #     print('execept')
-        #     return render(request,'home.html',context={'download':False,'error':True,'message':'Somthing goes wrong. Try again.'})
+                if int(video.length) > int(300) :
+                    return render(request,'home.html',context={'download':False,'error':True,'message':'Please select less than 5 minute'})
+                else:    
+                    bestaudio = video.getbestaudio(preftype='m4a')
+                    bestaudio.download(quiet=False, filepath=(f'{settings.MEDIA_ROOT}/{token}.m4a'))
+                    raw_audio = mp.AudioFileClip(os.path.join(settings.MEDIA_ROOT, f'{token}.m4a'))
+                    raw_audio.write_audiofile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))
+                    os.remove(os.path.join(settings.MEDIA_ROOT, f'{token}.m4a'))
+                    audio = sr.AudioFile(os.path.join(settings.MEDIA_ROOT, f'{token}.wav'))
+                    r = sr.Recognizer()
+                    with audio as source:
+                        r.adjust_for_ambient_noise(source)  
+                        audio_file = r.record(source)
+                    text = r.recognize_google(audio_file,language=request.POST.get('lang'))
+                    
+                    with open(os.path.join(settings.MEDIA_ROOT, f'{token}.txt'),mode ='w',encoding="utf-8") as file: 
+                        wrapped = textwrap.fill(text, 100)
+                        file.write(wrapped)
+                        
+                    return render(request,'home.html', context={'download':True,'token':token, 'error':False,'filename': video.title})
+        except:
+            print('execept')
+            return render(request,'home.html',context={'download':False,'error':True,'message':'Somthing goes wrong. Try again.'})
         
     return render(request,'home.html',context={'download':False,'error':False})
     
